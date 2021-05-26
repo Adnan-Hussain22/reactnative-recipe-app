13a765063b49dd50b197df70fb518e902b4d70cb
Adnan-Hussain22 <adnanrajput42@gmail.com>
2021-05-26 08:28:13 +0500

    add custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 import RootStackScreen from "./stacks/Decider";
+import { CUSTOM_FONTS } from "./constants/fonts";
 
 export default function App() {
   const [userToken, setUserToken] = React.useState("");
+  const [loaded] = useFonts({
+    [CUSTOM_FONTS.PROXIMA_REGULAR]: require("../assets/fonts/FontsFree-Net-pr10.ttf"),
+    [CUSTOM_FONTS.PROXIMA_SEMIBOLD]: require("../assets/fonts/FontsFree-Net-Proxima-Nova-Sbold.otf"),
+    [CUSTOM_FONTS.PROXIMA_SEMIBOLD_ITALIC]: require("../assets/fonts/FontsFree-Net-Proxima-Nova-Sbold-It.otf"),
+    [CUSTOM_FONTS.PROXIMA_BOLD]: require("../assets/fonts/FontsFree-Net-proxima_nova_bold-webfont.ttf"),
+  });
 
   React.useEffect(() => {
     setTimeout(() => {
       setUserToken("123");
     }, 200);
   });
+
+  if (!loaded) return null;
 
   return (
     <NavigationContainer>
---
+++ src/constants/fonts.ts
@@ -0,0 +1,6 @@
+export const CUSTOM_FONTS = {
+  PROXIMA_REGULAR: "ProximaRegular",
+  PROXIMA_BOLD: "ProximaBold",
+  PROXIMA_SEMIBOLD: "ProximaSemiBold",
+  PROXIMA_SEMIBOLD_ITALIC: "ProximaSemiBoldItalic",
+};
