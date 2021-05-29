092256fde8217bac3a63874c6a0238676eca75ae
adnandothussain <adnanrajput42@gmail.com>
2021-05-29 13:54:34 +0500

    signup wip

--- src/features/Signup/Signup.tsx
+++ src/features/Signup/Signup.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
-import { Text, View, StyleSheet, SafeAreaView } from "react-native";
-
-import { MaterialIcons } from "@expo/vector-icons";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 
 import Button from "src/components/Button";
 import { Logo } from "src/components/Svgs/Logo";
@@ -11,12 +9,12 @@ import Typography from "src/components/Typography/Typography";
 import { COLORS } from "src/constants/colors";
 import { moderateScale } from "src/utils/scale";
 import Icon from "src/components/Icon";
+import IconRoundButton from "src/components/Button/IconRoundButton";
 
 interface SignupProps {}
 
 const Signup: React.FC<SignupProps> = () => {
   return (
-    // <SafeAreaView style={styles.container}>
     <View style={styles.container}>
       <View
         style={{
@@ -49,22 +47,44 @@ const Signup: React.FC<SignupProps> = () => {
         round
         center
         width={moderateScale(140)}
-        style={{
-          marginTop: moderateScale(20),
-          paddingHorizontal: moderateScale(18),
-          paddingVertical: moderateScale(12),
-        }}
+        style={styles.createBtn}
         text="CREATE"
-        textStyle={{ color: COLORS.white, letterSpacing: moderateScale(2) }}
+        textStyle={styles.createTxt}
       />
-      <Typography
-        variant="BodyLight"
-        style={{ color: "#7e7e7e", textAlign: "center", marginTop: 20 }}
-      >
+      <Typography variant="BodyLight" {...styles.signupTxt}>
         Sign up now
       </Typography>
+      <View style={styles.socialIconContainer}>
+        <IconRoundButton
+          icon={
+            <Icon name="google" type="FontAwesome" style={styles.socialIcon} />
+          }
+          background={COLORS.googleOrange}
+          size={moderateScale(45)}
+        />
+        <IconRoundButton
+          icon={
+            <Icon
+              name="facebook-f"
+              type="FontAwesome"
+              style={styles.socialIcon}
+            />
+          }
+          background={COLORS.fbBlue}
+          size={moderateScale(45)}
+        />
+      </View>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Typography variant="BodyLight" color={COLORS.textGrey} marginRight={8}>
+          Have an account?
+        </Typography>
+        <TouchableOpacity>
+          <Typography variant="Body" color={COLORS.primaryRed}>
+            Sign In
+          </Typography>
+        </TouchableOpacity>
+      </View>
     </View>
-    // </SafeAreaView>
   );
 };
 
@@ -75,4 +95,18 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: moderateScale(15),
   },
+  createBtn: {
+    marginTop: moderateScale(20),
+    paddingHorizontal: moderateScale(18),
+    paddingVertical: moderateScale(12),
+  },
+  createTxt: { color: COLORS.white, letterSpacing: moderateScale(2) },
+  signupTxt: { color: COLORS.textGrey, textAlign: "center", marginTop: 20 },
+  socialIconContainer: {
+    width: 150,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignSelf: "center",
+  },
+  socialIcon: { color: COLORS.white, fontSize: moderateScale(25) },
 });
