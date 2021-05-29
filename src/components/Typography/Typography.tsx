9547540889cd938c2c5b689c9373f0ff5b416351
adnandothussain <adnanrajput42@gmail.com>
2021-05-29 13:27:17 +0500

    typography

---
+++ src/components/Typography/Typography.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Text, StyleSheet, TextStyle } from "react-native";
+import { CUSTOM_FONTS } from "src/constants/fonts";
+import { moderateScale } from "src/utils/scale";
+
+type variant = "H1" | "H2" | "BodyBold" | "BodySemiBold" | "Body" | "BodyLight";
+
+export interface TypographyProps {
+  style?: TextStyle;
+  variant?: variant;
+}
+
+const Typography: React.FC<TypographyProps> = ({
+  children,
+  style,
+  variant = "Body",
+}) => {
+  return <Text style={[styles[variant], style]}>{children}</Text>;
+};
+
+export default Typography;
+
+const styles = StyleSheet.create({
+  H1: {
+    fontFamily: CUSTOM_FONTS.PROXIMA_BOLD,
+    fontSize: moderateScale(26),
+  },
+  H2: {
+    fontFamily: CUSTOM_FONTS.PROXIMA_BOLD,
+    fontSize: moderateScale(23),
+  },
+  BodyBold: {
+    fontFamily: CUSTOM_FONTS.PROXIMA_BOLD,
+    fontSize: moderateScale(18),
+  },
+  BodySemiBold: {
+    fontFamily: CUSTOM_FONTS.PROXIMA_SEMIBOLD,
+    fontSize: moderateScale(18),
+  },
+  Body: {
+    fontFamily: CUSTOM_FONTS.PROXIMA_SEMIBOLD,
+    fontSize: moderateScale(16),
+  },
+  BodyLight: {
+    fontFamily: CUSTOM_FONTS.PROXIMA_REGULAR,
+    fontSize: moderateScale(16),
+  },
+});
