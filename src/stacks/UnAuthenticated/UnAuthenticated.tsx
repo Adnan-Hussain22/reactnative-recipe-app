c21d150878fc75902650b9820f26203203c061e6
adnandothussain <adnanrajput42@gmail.com>
2021-05-30 02:10:18 +0500

    unauth stack

--- src/stacks/UnAuthenticated/UnAuthenticated.tsx
+++ src/stacks/UnAuthenticated/UnAuthenticated.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Onboarding from "src/features/Onboarding";
+
+import Signup, { PersonalInfo, ProfileInfo } from "src/features/Signup";
+import { UNAUHTENTICATED_ROUTES } from "src/constants/Routes";
 
 const Stack = createStackNavigator();
 
 const UnAuthenticatedStack = () => {
   return (
-    <Stack.Navigator headerMode="none" initialRouteName="Onboarding">
-      {/* <Stack.Screen name="Login" component={HomeScreen} /> */}
-      <Stack.Screen name="Onboarding" component={Onboarding} />
+    <Stack.Navigator
+      headerMode="none"
+      initialRouteName={UNAUHTENTICATED_ROUTES.SIGNUP}
+    >
+      <Stack.Screen name={UNAUHTENTICATED_ROUTES.SIGNUP} component={Signup} />
+      <Stack.Screen
+        name={UNAUHTENTICATED_ROUTES.PERSONAL_INFO}
+        component={PersonalInfo}
+      />
+      <Stack.Screen
+        name={UNAUHTENTICATED_ROUTES.PROFILE_INFO}
+        component={ProfileInfo}
+      />
     </Stack.Navigator>
   );
 };
