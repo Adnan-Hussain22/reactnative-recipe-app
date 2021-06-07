3cf64b51dc3108f46bfab2447e93770b199a82c6
adnandothussain <adnanrajput42@gmail.com>
2021-06-07 11:38:13 +0500

    reduce bottom tab label

--- src/constants/navigatorOptions.ts
+++ src/constants/navigatorOptions.ts
@@ -3,11 +3,13 @@ import { Platform } from "react-native";
 import { BOTTOM_TAB_BAR_HEIGHT, BOTTOM_TAB_BAR_PADDING_BOTTOM } from "./common";
 import { COLORS } from "./colors";
 import { typographyStyles } from "./globalStyles";
+import { moderateScale } from "src/utils/scale";
 
 export const tabBarOptions = {
   activeTintColor: COLORS.primaryRed,
   labelStyle: {
     ...typographyStyles.P,
+    fontSize: moderateScale(11),
     marginTop: 8,
   },
   inactiveTintColor: COLORS.textGrey,
