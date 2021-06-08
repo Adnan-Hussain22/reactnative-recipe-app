de840e8c2d092a01fda9ad8b71f76b9e90d505ba
adnandothussain <adnanrajput42@gmail.com>
2021-06-08 11:38:05 +0500

    discover list mock

---
+++ src/features/Discover/data.ts
@@ -0,0 +1,96 @@
+import { IDiscoverList } from "src/typings/discover";
+
+export const DISCOVER_DATA: IDiscoverList[] = [
+  {
+    index: 0,
+    title: "Top Picks",
+    horizontal: true,
+    type: 0,
+    data: [
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+    ],
+  },
+  {
+    index: 1,
+    title: "Recently Created",
+    horizontal: true,
+    type: 0,
+    data: [
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+        subTitle: "Lorem Ipsum",
+      },
+    ],
+  },
+  {
+    index: 2,
+    title: "By Cuisine",
+    horizontal: true,
+    type: 1,
+    data: [
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+      },
+      {
+        title: "Hampden-Sydney",
+        image:
+          "https://www.foodies.pk/wp-content/uploads/2020/04/italian-cuisine-italian-food-scaled.jpeg",
+      },
+    ],
+  },
+];
