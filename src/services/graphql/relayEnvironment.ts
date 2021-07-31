dc88b6251f8a252e0cbcfad48f16e9151c4c50e1
Adnan Hussain <adnanrajput42@gmail.com>
2021-07-31 20:40:10 +0500

    fix: relay env

--- src/services/graphql/relayEnvironment.ts
+++ src/services/graphql/relayEnvironment.ts
@@ -1,12 +1,12 @@
 import { Environment, RecordSource, Store } from "relay-runtime";
 import {
   authMiddleware,
-  errorMiddleware,
   RelayNetworkLayer,
   urlMiddleware,
-  loggerMiddleware,
   MiddlewareNextFn,
   RelayRequestAny,
+  errorMiddleware,
+  loggerMiddleware,
 } from "react-relay-network-modern";
 import { Config } from "src/constants/config";
 
@@ -29,10 +29,12 @@ const network = new RelayNetworkLayer(
 
     authMiddleware({
       token: () => {
+        const token =
+          "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdGFydE9aZXJvLmNvbSIsImF1ZCI6InN0YXJ0T1plcm91c2VyIiwic3ViIjoiNjBlYjA0NTgyMGRiNTFlNDllZTU0YzNkIiwiaWF0IjoxNjI2MDE0ODA5LCJleHAiOjE2Mjg2MDY4MDksInBybSI6IjE0OGY3MDQwZjgwNTNlYzgyZWUxNTQ1MWYwOGFiZTI3YTY1YjY2ZTlkZTI3Mzg4ZTQyZGRjZjczMGQ5MTVmODRjMTczM2ZmNjMxNjIzMTdlNmEyNzg1MTI4MzEwN2Y4NzJmYzhhM2RiY2IzMDg3ZjM2Nzk1MzQ2MDUyNGQxMjhmIn0.jJ4UEayoqauPL4gvo_tTw6m9eWVYOVhyZ4SJdAwo4EdWXFNtN7dsp9QYpfgcfDL88_4DNC8H4o98keH2Tqbjmg";
         // Todo replace the token get logic using context
-        return "Bearer Token";
+        return token;
       },
-      prefix: "",
+      prefix: "Bearer ",
     }),
     __DEV__ ? loggerMiddleware() : null,
     __DEV__ ? errorMiddleware() : null,
